5b02374df203f6690114c10051d649d31b7431ae
jjayeon <autumn.chiu.136@gmail.com>
2021-08-02 14:53:42 -0400

    gutting blog-specific things, replacing with tonetype stuff

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request
 
 # from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
-from flaskext.markdown import Markdown
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -16,7 +15,6 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-Markdown(app)
 # app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 app.config[
     "SQLALCHEMY_DATABASE_URI"
@@ -31,7 +29,6 @@ app.config[
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 class UserModel(db.Model):
     __tablename__ = "users"
 
@@ -45,94 +42,21 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-
-f = open("./lavina.json")
-data = json.load(f)
-f.close()
-
-POSTDIR = "./posts/"
-
-
 @app.route("/")
-@app.route("/blog/")
-@app.route("/blog/<post>")
-def blog(post=None):
-    p = re.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}.+\\.md$")
-    posts = list(filter(p.match, os.listdir(POSTDIR)))
-    posts.sort()
-    posts.reverse()
-    post_titles = []
-    for s in posts:
-        with open(POSTDIR + s) as f:
-            post_titles.append(f.readline().replace("## ", "").strip())
-    text = ""
-    title = "Autumn Chiu"
-
-    if request.path == "/":
-        post = posts[0]
-    if post is not None:
-        posts = posts[:5]
-        with open(POSTDIR + post, "r") as f:
-            text = f.read()
-            title = text.partition("\n")[0].replace("## ", "") + " | " + title
+def index():
     return render_template(
         "blog.html",
-        title=title,
-        text=text,
-        posts=posts,
-        post_titles=post_titles,
+        title="Test Page",
+        text="Test page for now.  Will make new page soon.",
     )
-
 
 @app.route("/health/")
 def health():
     return "im healthy!"
 
-
-# mlh routes
-
-
-@app.route("/mlh/")
-@app.route("/mlh/Experience/")
-def index():
-    return render_template(
-        "data.html",
-        data=data,
-        main="Experience",
-        side1="Projects",
-        side2="Accomplishments",
-        url=os.getenv("URL"),
-    )
-
-
-@app.route("/mlh/Projects/")
-def projects():
-    return render_template(
-        "data.html",
-        data=data,
-        main="Projects",
-        side1="Experience",
-        side2="Accomplishments",
-        url=os.getenv("URL"),
-    )
-
-
-@app.route("/mlh/Accomplishments/")
-def accomplishments():
-    return render_template(
-        "data.html",
-        data=data,
-        main="Accomplishments",
-        side1="Experience",
-        side2="Projects",
-        url=os.getenv("URL"),
-    )
-
-
 # database shenanigans
 
-
-@app.route("/mlh/register/", methods=("GET", "POST"))
+@app.route("/register/", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
         username = request.form.get("username")
@@ -157,7 +81,7 @@ def register():
     return render_template("login.html", data=data, mode="register")
 
 
-@app.route("/mlh/login/", methods=("GET", "POST"))
+@app.route("/login/", methods=("GET", "POST"))
 def login():
     if request.method == "POST":
         username = request.form.get("username")
