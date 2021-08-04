0c7afe446d634bbfd2abf016f15e287713c105da
jjayeon <autumn.chiu.136@gmail.com>
2021-08-04 12:34:24 -0400

    added session tracking

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 # from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,8 +12,8 @@ from flask_migrate import Migrate
 # load_dotenv()
 
 app = Flask(__name__)
+app.secret_key="abc"
 app.url_map.strict_slashes = False
-# app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
@@ -43,11 +43,14 @@ class UserModel(db.Model):
 
 
 @app.route("/")
+@app.route("/play")
 def index():
+    username=""
+    if "username" in session:
+        username=session["username"]
     return render_template(
-        "blog.html",
-        title="Test Page",
-        text="Test page for now.  Will make new page soon.",
+        "layout.html",
+        username=username
     )
 
 
@@ -56,10 +59,18 @@ def health():
     return "im healthy!"
 
 
-# database shenanigans
+# user shenanigans
 
+@app.route("/user/")
+def user():
+    return 'TODO: make user page <form action="/logout/" method="post" target="_self"> <input type="submit" value="logout"> </form> <a href="/">home</a>'
 
-@app.route("/register/", methods=("GET", "POST"))
+@app.route("/logout/", methods=["POST"])
+def logout():
+    session.pop("username")
+    return 'logout successful. <a href="/">home</a>'
+
+@app.route("/user/register/", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
         username = request.form.get("username")
@@ -67,24 +78,26 @@ def register():
         error = None
 
         if not username:
-            error = "Username is required."
+            error = 'Username is required. <a href="/">home</a>'
         elif not password:
-            error = "Password is required."
+            error = 'Password is required. <a href="/">home</a>'
         elif UserModel.query.filter_by(username=username).first() is not None:
-            error = f"User {username} is already registered."
+            error = f'User {username} is already registered. <a href="/">home</a>'
 
         if error is None:
             new_user = UserModel(username, generate_password_hash(password))
             db.session.add(new_user)
             db.session.commit()
-            return f"User {username} created successfully"
+            return f'User {username} created successfully. <a href="/">home</a>'
         else:
             return error, 418
-    # TODO: return a register page
-    return render_template("login.html", mode="register")
+    username=""
+    if "username" in session:
+        username=session["username"]
+    return render_template("login.html", mode="register", username=username)
 
 
-@app.route("/login/", methods=("GET", "POST"))
+@app.route("/user/login/", methods=("GET", "POST"))
 def login():
     if request.method == "POST":
         username = request.form.get("username")
@@ -94,13 +107,17 @@ def login():
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = "Incorrect username."
+            error = 'Incorrect username. <a href="/">home</a>'
         elif not check_password_hash(user.password, password):
-            error = "Incorrect password."
+            error = 'Incorrect password. <a href="/">home</a>'
 
         if error is None:
-            return "Login successful", 200
+            session["username"] = username
+            return 'Login successful. <a href="/">home</a>', 200
         else:
             return error, 418
         # TODO: Return a login page
-    return render_template("login.html", mode="login")
+    username=""
+    if "username" in session:
+        username=session["username"]
+    return render_template("login.html", mode="login", username=username)
