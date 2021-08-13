35521381a95384def03d27c969241ce0feca0369
autumn <autumn.chiu.136@gmail.com>
2021-08-13 11:22:08 -0400

    update index.js to read from db

--- app/static/index.js
+++ app/static/index.js
@@ -4,8 +4,7 @@ window.onload = function () {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         var song_name = document.getElementById("song_name");
-        // xobj.open('GET', '/api' + song_name, true);
-        xobj.open('GET', '../static/song-data.json', true);
+        xobj.open('GET', '/api/' + song_name, true);
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 callback(xobj.responseText);
