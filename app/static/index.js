d9bdb1fda574569bf0adb08251743a7ad7be1f94
Jocelyn Heckenkamp <jocelynheckenkamp@gmail.com>
2021-08-17 23:57:05 -0500

    Add a5

--- app/static/index.js
+++ app/static/index.js
@@ -4,8 +4,8 @@ window.onload = function () {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         var song_name = document.getElementById("song_name").innerHTML;
-        xobj.open('GET', '/api/' + song_name, true);
-        //xobj.open('GET', '../static/can-can.json', true);
+        //xobj.open('GET', '/api/' + song_name, true);
+        xobj.open('GET', '../static/flight-of-the-bumblebee.json', true);
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 callback(xobj.responseText);
